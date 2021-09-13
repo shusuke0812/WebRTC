c486bead60621800ee6f0fd68b3be0caeca696cc
shusuke0812 <shu.ota0812@gmail.com>
2021-09-14 00:43:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Study for sampel application using WebRTC
+
+## Summary
+
+making ....
+
+## Getting Started
+
+
+## Development Guidelines
+
+- Xcode 12.5.1 （12E507）
+- Swift 5.0
+- CocoaPods 1.9.3
+- MacOS BigSur 11.4
+- Target iOS 13.0
+
+## References
+- 
---
+++ WebRTC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebRTC
+//
+//  Created by Shusuke Ota on 2021/9/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
